02d3eed401e21034276df384cb1b190dcb710bc2
kilian3309 <kiki.jugie@gmail.com>
2019-10-24 18:42:42 +0200

    refactoring kpreprocessor.h

--- src/kPreprocessor.h
+++ src/kPreprocessor.h
@@ -19,7 +19,7 @@
 	- UTILS
 	- VERSION
  
- Each module define K_H_[MODULE] if present. Each module
+ Each module define K_PP_[MODULE] if present. Each module
  also have a KVER-like version which can be handled with
  VERSION module (c.f VERSION module). The version of
  each module is defined by K_VER_H_[MODULE].
@@ -64,24 +64,23 @@
 		(return 1 for x=0 and 0 for x=1)
  */
 
-#define K_H_UTILS
+#define K_PP_UTILS
 
-#define getLastOf(arr) (arr)[(arr).size()-1]
+#define K_GETLASTOF(arr) (arr)[(arr).size()-1]
 
-#define KCAT(x, y) x##y
-#define KSTR(x) #x
-#define KTOKSTR(x) KCAT(x, _k)
-#define KCOMMA() ,
-#define KDOT() .
-#define KEMPTY()
-#define KARGEND() 0
+#define K_CAT(x, y) x##y
+#define K_STR(x) #x
+#define K_COMMA() ,
+#define K_DOT() .
+#define K_EMPTY()
+#define K_ARGEND() 0
 
-#define KDEFER1(m) m KEMPTY()
-#define KDEFER2(m) m KEMPTY KEMPTY()()
-#define KDEFER3(m) m KEMPTY KEMPTY KEMPTY()()()
-#define KDEFER4(m) m KEMPTY KEMPTY KEMPTY KEMPTY()()()()
+#define K_DEFER_1(m) m K_EMPTY()
+#define K_DEFER_2(m) m K_EMPTY K_EMPTY()()
+#define K_DEFER_3(m) m K_EMPTY K_EMPTY K_EMPTY()()()
+#define K_DEFER_4(m) m K_EMPTY K_EMPTY K_EMPTY K_EMPTY()()()()
 
-#define EVAL(...) EVAL1024(__VA_ARGS__)
+#define K_EVAL(...) EVAL1024(__VA_ARGS__)
 #define EVAL1024(...) EVAL512(EVAL512(__VA_ARGS__))
 #define EVAL512(...) EVAL256(EVAL256(__VA_ARGS__))
 #define EVAL256(...) EVAL128(EVAL128(__VA_ARGS__))
@@ -94,30 +93,28 @@
 #define EVAL2(...) EVAL1(EVAL1(__VA_ARGS__))
 #define EVAL1(...) __VA_ARGS__
 
-#define KU_IF_ELSE(c) _IF_ ## c
+#define K_IF_ELSE(c) _IF_ ## c
 #define _IF_1(...) __VA_ARGS__ _IF_1_ELSE
 #define _IF_0(...) _IF_0_ELSE
 
 #define _IF_1_ELSE(...)
 #define _IF_0_ELSE(...) __VA_ARGS__
 
-#define KU_FIRST(a,...) a
-#define KU_SECOND(a,b,...) b
-#define KU_THIRD(a,b,c,...) c
+#define K_FIRST(a,...) a
+#define K_SECOND(a,b,...) b
+#define K_THIRD(a,b,c,...) c
 
-#define KU_ISOLATEONE(a,...) __VA_ARGS__
-#define KU_ISOLATETWO(a,b,...) __VA_ARGS__
-#define KU_ISOLATETREE(a,b,c,...) __VA_ARGS__
+#define K_ISOLATEONE(a,...) __VA_ARGS__
+#define K_ISOLATETWO(a,b,...) __VA_ARGS__
+#define K_ISOLATETREE(a,b,c,...) __VA_ARGS__
 
-#define _KU_IS_PROBE(...) EVAL2(KU_SECOND(__VA_ARGS__, 0))
-#define _KU_PROBE() ~,1 //'~' is arbitrary
+#define _K_IS_PROBE(...) EVAL2(K_SECOND(__VA_ARGS__, 0))
+#define _K_PROBE() ~,1 //'~' is arbitrary
 
-#define KU_NOT(x) _KU_IS_PROBE(KCAT(_KU_NOT_,x))
-#define _KU_NOT_0 _KU_PROBE()
+#define K_NOT(x) _K_IS_PROBE(K_CAT(_K_NOT_,x))
+#define _K_NOT_0 _K_PROBE()
 
-#define KFAILED(x) x!=0
-
-#define K_VER_H_UTILS 1,0,06
+#define K_VER_PP_UTILS 1,0,06
 
 /*
 ----------									  ----------
@@ -147,66 +144,60 @@
  with version number separated by dot.
  KVER_STR(EXAMPLE_VERSION) will become "1.2.3"
  */
-#define K_H_VERSION
+#define K_PP_VERSION
 
-#define KVER_MAJ(v) EVAL(KU_FIRST(v))
+#define KVER_MAJ(v) K_EVAL(K_FIRST(v))
 #define _KVER_MAJ(mj,mi,ed) mj
-#define KVER_MIN(v) EVAL(KU_SECOND(v))
+#define KVER_MIN(v) K_EVAL(K_SECOND(v))
 #define _KVER_MIN(mj,mi,ed) mi
-#define KVER_EDIT(v) EVAL(KU_THIRD(v))
+#define KVER_EDIT(v) K_EVAL(K_THIRD(v))
 #define _KVER_EDIT(mj,mi,ed) ed
 
-#define _KVER_NUM_F(...) KCAT(_KVER_MAJ(__VA_ARGS__), _KVER_MIN(__VA_ARGS__))
-#define _KVER_NUM_S(...) KCAT(_KVER_EDIT(__VA_ARGS__),) //Cleaner : eval ?
-#define KVER_NUM(v) EVAL(KCAT(_KVER_NUM_F(v),_KVER_NUM_S(v)))
+#define _KVER_NUM_F(...) K_CAT(_KVER_MAJ(__VA_ARGS__), _KVER_MIN(__VA_ARGS__))
+#define _KVER_NUM_S(...) K_CAT(_KVER_EDIT(__VA_ARGS__),) //Cleaner : eval ?
+#define KVER_NUM(v) K_EVAL(K_CAT(_KVER_NUM_F(v),_KVER_NUM_S(v)))
 
-#define _KVER_STR_F1(...) EVAL2(KDEFER1(KCAT)(KDOT(),_KVER_MIN(__VA_ARGS__)))
-#define _KVER_STR_F(...) KCAT(_KVER_MAJ(__VA_ARGS__),_KVER_STR_F1(__VA_ARGS__))
-#define _KVER_STR_S(...) KCAT(KDOT(),_KVER_EDIT(__VA_ARGS__))
-#define KVER_STR(v) EVAL1(KDEFER1(KSTR)(EVAL(KCAT(_KVER_STR_F(v), _KVER_STR_S(v)))))
+#define _KVER_STR_F1(...) EVAL2(K_DEFER_1(K_CAT)(K_DOT(),_KVER_MIN(__VA_ARGS__)))
+#define _KVER_STR_F(...) K_CAT(_KVER_MAJ(__VA_ARGS__),_KVER_STR_F1(__VA_ARGS__))
+#define _KVER_STR_S(...) K_CAT(K_DOT(),_KVER_EDIT(__VA_ARGS__))
+#define KVER_STR(v) EVAL1(K_DEFER_1(K_STR)(K_EVAL(K_CAT(_KVER_STR_F(v), _KVER_STR_S(v)))))
 
-#define K_VER_H_VERSION 2,0,02
+#define K_VER_PP_VERSION 2,0,02
 
+ /*
+ ----------									  ----------
+  **********     kLib annotation system      **********
+ ----------									  ----------
+ TODO: Doc
+ */
 
-#define K_H_LOOP
-#define K_LOOP_H_VERSION 1,0,0
-
-#define _KFOREACH(m, ...)  \
-		KDEFER1(KU_IF_ELSE)(KU_NOT(KU_FIRST(__VA_ARGS__)))()(\
-		EVAL1(KDEFER1(m)(KU_FIRST(__VA_ARGS__))) \
-		KDEFER1(__KFOREACH)(m,KU_ISOLATEONE(__VA_ARGS__),KARGEND()))
-
-#define __KFOREACH(m, ...) _KFOREACH(m,__VA_ARGS__)
-
-#define KFOREACH(macro, args, ...) EVAL(_KFOREACH(macro, args, __VA_ARGS__, KARGEND())) 
-
-#define K_VER_H_ANNOTE 1,0,0
+#define K_VER_H_ANNOTE 1,0,1
 #ifndef K_AN_DEV_KLIB_DLEVEL
 #define K_AN_DEV_KLIB_DLEVEL 0
 #endif // !K_AN_DEV_KLIB_DLEVEL
 
-
-#define __k_an(t, c, ...) __pragma(message(__FILE__"("EVAL2(KDEFER1(KSTR)(__LINE__))"): "#t" "#c": "__VA_ARGS__))
-#define _k_e_an(c, ...) __k_an(error, c, __VA_ARGS__)
-#define _k_w_an(c, ...) __k_an(warning, c, __VA_ARGS__)
-#define _k_i_an(c, ...) __k_an(message,  , __VA_ARGS__)
-#define _k_an_add_p(p, an, c, ...)  EVAL2(KDEFER2(an)(c, p __VA_ARGS__))
+#define _K_AN_BASE(t, c, ...) __pragma(message(__FILE__"("EVAL2(K_DEFER_1(K_STR)(__LINE__))"): "#t" "#c": "__VA_ARGS__))
+#define _K_AN_ERROR(c, ...) _K_AN_BASE(error, c, __VA_ARGS__)
+#define _K_AN_WARNING(c, ...) _K_AN_BASE(warning, c, __VA_ARGS__)
+#define _K_AN_MESSAGE(c, ...) _K_AN_BASE(message,  , __VA_ARGS__)
+#define _K_AN_ADD_PREFIX(p, an, c, ...)  EVAL2(K_DEFER_2(an)(c, p __VA_ARGS__))
 #ifdef K_AN_DEV_KLIB
-#define __k_kd_an_wa_prefix() "To work around: "
-#define _k_an_kd_wwa(...)  _k_an_add_p(__k_kd_an_wa_prefix(), _k_w_an, WA, __VA_ARGS__)
-#define _k_an_kd_ewa(...) _k_an_add_p(__k_kd_an_wa_prefix(), _k_e_an, WA, __VA_ARGS__)
-#define _k_an_kd_iwa(...) _k_an_add_p(__k_kd_an_wa_prefix(), _k_i_an, WA, __VA_ARGS__)
+//TODO: Rework this from scratch with new prefix system
+#define _K_AN_KLIBDEV_WORKAROUND_PREFIX() "To work around: "
+#define _K_AN_KLIBDEV_WARNWA(...)  _K_AN_ADD_PREFIX(_K_AN_KLIBDEV_WORKAROUND_PREFIX(), _K_AN_WARNING, WA, __VA_ARGS__)
+#define _K_AN_KLIBDEV_ERRWA(...) _K_AN_ADD_PREFIX(_K_AN_KLIBDEV_WORKAROUND_PREFIX(), _K_AN_ERROR, WA, __VA_ARGS__)
+#define _K_AN_KLIBDEV_MSGWA(...) _K_AN_ADD_PREFIX(_K_AN_KLIBDEV_WORKAROUND_PREFIX(), _K_AN_MESSAGE, WA, __VA_ARGS__)
 
 #if K_AN_DEV_KLIB_DLEVEL > 0
-#define _k_an_kd_dtodo(...) _k_an_add_p(" TODO: ",_k_i_an,TODO,__VA_ARGS__)
+#define _K_AN_KLIBDEV_TODO(...) _K_AN_ADD_PREFIX(" TODO: ",_K_AN_MESSAGE,TODO,__VA_ARGS__)
 #else
-#define _k_an_kd_dtodo(c,...)
+#define _K_AN_KLIBDEV_TODO(c,...)
 #endif // K_AN_DEV_KLIB_DLEVEL > 0
 #else
-#define _k_an_kd_wwa(...)
-#define _k_an_kd_ewa(...)
-#define _k_an_kd_iwa(...)
-#define _k_an_kd_dtodo(...)
+#define _K_AN_KLIBDEV_WARNWA(...)
+#define _K_AN_KLIBDEV_ERRWA(...)
+#define _K_AN_KLIBDEV_MSGWA(...)
+#define _K_AN_KLIBDEV_TODO(...)
 #endif
 
 
